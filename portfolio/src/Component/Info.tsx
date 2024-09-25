34304f172b86215cebf1a0d57b9e6ae533b53942
dpclfk <lee@iseungbaeui-MacBookAir.local>
2024-09-25 15:04:30 +0900

    add:project url

--- portfolio/src/Component/Info.tsx
+++ portfolio/src/Component/Info.tsx
@@ -4,10 +4,10 @@ const Info = (): JSX.Element => {
   return (
     <div className="flex h-screen bg-infocolor">
       <div className="w-[50%] flex justify-center items-center">
-        <img className="w-[60%] h-[70%] border" src={info} alt="내 사진" />
+        <img className="w-[52vh] h-[70vh] border min-w-[52vh]" src={info} alt="내 사진" />
       </div>
-      <div className="w-[50%] flex items-center">
-        <div className="text-7xl">
+      <div className="w-[50%] flex items-center overflow-hidden">
+        <div className="text-7xl ">
           항상 정진하는 개발자 <br />
           <br />
           이승배입니다.
--- portfolio/src/Component/Portfolio.tsx
+++ portfolio/src/Component/Portfolio.tsx
@@ -6,16 +6,36 @@ const Portfolio = (): JSX.Element => {
     <>
       <div className="w-full h-screen bghalf">
         <div className="pt-8 pl-20 text-5xl">Portfolio</div>
-        <div className="flex items-center justify-evenly h-[90%] overflow-hidden">
+        <div className="flex items-center justify-evenly h-[90%] overflow-hidden pb-20">
           <div className="w-[35%] h-[40%] mb-4">
-            <img className="w-full h-full border" src={market} alt="market" />
+            <div className="text-3xl text-center font-bold pb-4">
+              이미지 클릭시 프로젝트 사이트로 이동
+            </div>
+            <img
+              className="w-full h-full border cursor-pointer"
+              src={market}
+              alt="market"
+              onClick={() => {
+                window.open("https://www.dpclfk.com/market/");
+              }}
+            />
             <div className="text-3xl text-center font-bold pt-4">
               미션가이드에 따라 택배사가 <br />
               직거래 하는 사이트 제작
             </div>
           </div>
           <div className="w-[35%] h-[40%] mb-4">
-            <img className="w-full h-full border" src={calc} alt="calc" />
+            <div className="text-3xl text-center font-bold pb-4">
+              이미지 클릭시 프로젝트 사이트로 이동
+            </div>
+            <img
+              className="w-full h-full border cursor-pointer"
+              src={calc}
+              alt="calc"
+              onClick={() => {
+                window.open("https://lostarkcalc.dpclfk.com/");
+              }}
+            />
             <div className="text-3xl text-center font-bold pt-4">
               게임 아이템 제작시 이득 및 손해를 <br />
               확인할수있는 사이트 제작
